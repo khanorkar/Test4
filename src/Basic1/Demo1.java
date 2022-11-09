e8824de1ca231f1806752320eda031555285ed60
Khanorkar <Khanorkar@DESKTOP-JM76D50>
2022-11-09 15:48:47 +0530

    Commit Second 2

---
+++ src/Basic1/Demo1.java
@@ -0,0 +1,16 @@
+package Basic1;
+
+public class Demo1 
+{
+public static void main(String[] args)
+{
+	System.out.println("Hii khanorkar Family");
+	
+	Lax();
+}
+
+public static void Lax()
+{
+	System.out.println("Kasa kai Nagpur");
+}
+}
